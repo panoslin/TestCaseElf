11a67265d41e88192267c978af58dc35670fa536
Panos <panoslin@foxmail.com>
2022-01-09 17:33:28 +0800

    add: pipe.py

---
+++ test_case_elf/pipe.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by panos on 2022/1/9
+# IDE: PyCharm
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by panos on 2022/1/7
+# IDE: PyCharm
+import json
+import pandas as pd
+# typing
+from pathlib import Path
+from typing import (
+    Union,
+)
+
+
+class Pipe(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __ror__(self, other):
+        return self.func(other)
+
+    def __call__(self, *args, **kwargs):
+        return Pipe(lambda x: self.func(x, *args, **kwargs))
+
+
+@Pipe
+def to_json(iterable):
+    return json.dumps(iterable)
+
+
+@Pipe
+def to_df(iterable):
+    return pd.DataFrame(iterable)
+
+
+@Pipe
+def to_csv(iterable, path: Union[str, Path], **kwargs):
+    return pd.DataFrame(iterable).to_csv(path, index=False, **kwargs)
